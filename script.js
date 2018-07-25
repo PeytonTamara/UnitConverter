c99ba97a39a97c8e70b06a9fe21e196332b3ae67
DePaul <CONF0580@depaul.edu>
2018-07-25 11:39:44 -0500

    finished!

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
 	
 	$("button").click( function() {
-		$("")
+		
+		var number = $("#number_inches").val();
+		
+		console.log(number);
+		
+		var sentence = parseInt(number) / 12.0;
+		console.log(sentence);
+		
+		$("#result").html(sentence);
 		
 	});
-
-
-
+	
+	$("button").click( function() {
+		
+		var number2 = $("#number_feet").val();
+		
+		console.log(number2);
+		
+		var sentence = parseInt(number2) * 12.0;
+		console.log(sentence);
+		
+		$("#result2").html(sentence);
+	});
 
 });
